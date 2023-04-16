8b620281abee3609a8d12a428bc8a335718c702e
Amipriya22 <130905751+Amipriya22@users.noreply.github.com>
2023-04-16 17:26:21 +0530

    Add files via upload

---
+++ FirstPython.py
@@ -0,0 +1,78 @@
+import math as m
+
+
+class Car():
+
+    # attributes of this Car class
+    def __init__(self,make,model,year,speed,x,y,angle):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.velocity = (self.speed *m.cos(self.angle), self.speed *m.sin(self.angle))
+
+    # Methods of this Car class
+    def acclerate(self,speed_increment):
+        self.speed += speed_increment
+
+    def brake(self,speed_decrement):
+        self.speed -= speed_decrement
+
+    def move(self,dt):
+        self.x += self.speed*m.cos(m.radians(self.angle))*dt
+        self.y += self.speed*m.sin(m.radians(self.angle))*dt
+
+    def detect_collision(self,car2):
+        relative_distance = m.sqrt(((self.x-car2.x)**2) +((self.y-car2.y)**2))
+        if relative_distance<0.1 :
+            return True
+        else:
+            return False
+
+    def time_to_collision(self, car2):
+        if self.detect_collision(car2):
+            return None
+
+        angle_diff = abs(math.atan2(*np.cross(self.velocity, car2.velocity)) % (math.pi))
+
+
+        if angle_diff == 0 or angle_diff == math.pi:
+            return None  # no time to collision
+
+        a = (self.velocity[0] - car2.velocity[0]) ** 2 + (self.velocity[1] - car2.velocity[1]) ** 2
+        b = 2 * ((self.velocity[0] - car2.velocity[0]) * (self.position[0] - car2.position[0]) +
+                 (self.velocity[1] - car2.velocity[1]) * (self.position[1] - car2.position[1]))
+        c = (self.position[0] - car2.position[0]) ** 2 + (self.position[1] - car2.position[1]) ** 2
+
+        discriminant = b ** 2 - 4 * a * c
+        if discriminant < 0:
+            return None
+        else:
+            sqrt_discriminant = math.sqrt(discriminant)
+            t1 = (-b + sqrt_discriminant) / (2 * a)
+            t2 = (-b - sqrt_discriminant) / (2 * a)
+            if t1 > 0:
+                return t1  # this is the time to collision
+
+
+
+
+
+#_main_
+
+#
+# car1 =Car("tesla","model 3",2022,20,0,0,m.pi/4)
+# car2 =Car("auto","model 2",2022,30,2,2,3*(m.pi)/4)
+#
+# car1.brake(10)
+# print(car1.speed)
+# car1.acclerate(12)
+# print(car1.speed)
+# print(car1.velocity)
+# print(car1.detect_collision(car2))
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# WEB-3.0
+
+
+# WEB 3.O RECRUITMENT TASK(Car Collision Simulator)
+
+This is a Python program that simulates the movement and collision of two cars using a simple physics model.
+
+## Table of Contents
+
+- [How it works](#how-it-works)
+- [How to run it](#how-to-run-it)
+- [How to modify it](#how-to-modify-it)
+- [License](#license)
+
+## How it works
+
+The program defines a Car class that has the following attributes and methods:
+
+- Attributes: make, model, year, speed, x, y, angle, velocity
+- Methods: accelerate, brake, move, detect_collision, time_to_collision
+
+The Car class uses the math module to perform calculations involving trigonometry and vectors.
+
+The main program creates two instances of the Car class with different initial values and moves them for a certain amount of time. It then checks if they have collided or not, and prints the result.
+
+## How to run it
+
+To run the program, you need to have Python 3 installed on your system. You can download it from https://www.python.org/downloads/.
+
+You can run the program from the command line by typing:
+
+`python car_collision_simulator.py`
+
+You can also run the program from an IDE or editor of your choice that supports Python.
+
+## How to modify it
+
+You can modify the program by changing the values of the variables in the main program. For example, you can change the make, model, year, speed, x, y, angle of the cars, or the duration of the simulation.
+
+You can also modify the Car class by adding new attributes or methods, or changing the existing ones. For example, you can add a color attribute to the Car class, or change the formula for calculating the velocity.
+
+## License
+
+This program is free and open source. You can use it for any purpose, as long as you give credit to the original author.
